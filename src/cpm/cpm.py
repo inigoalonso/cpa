f34934fcf1518cabeff910c0df1f1c424f721c2f
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2021-10-13 23:18:34 +0200

    reordering

---
+++ src/cpm/cpm.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class something():
+
+    def __init__(self,one,two=''):
+        """
+        Silly example
+        
+        Parameters
+        ----------
+        one : uno
+        two : dos
+        """
+
+        self.one = one
+        self.two = two
