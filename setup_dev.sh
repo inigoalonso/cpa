e6c03f0da117334491e37bb063caf490a14587f3
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2021-10-14 09:50:52 +0200

    fixed line endings

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,14 +1,11 @@
 # Setup python virtual environment
-
-python -m venv .venv
+python3 -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Flit local installation in virtual environment
-
 flit build
 flit install --deps=develop
 
 # build the documentation
-
 sphinx-autobuild docs/source docs/build/html
